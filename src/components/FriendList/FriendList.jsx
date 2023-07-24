ccea38cc90b0ccbe6d7ccce4e4bccd90da21ddf3
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-07-24 22:25:41 +0300

    Add component FriendList

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Fragment } from 'react';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+import css from './FriendList.module.css'
+
+const FriendList = ({friends})=> {
+    return (<ul className={css.friendList}>
+               {friends.map(friend => {
+                   return (<Fragment key={friend.id}>
+                   <FriendListItem
+                       avatar={friend.avatar}
+                       name={friend.name}
+                       isOnline={friend.isOnline}
+                       id={friend.id}
+                   /></Fragment>)
+                   
+               })}
+        </ul>)
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.object.isRequired),
+}
+
+export default FriendList;
